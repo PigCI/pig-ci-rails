cc7cbdb3e80097c823dadd626eab0920475511a3
Mike Rogers <me@mikerogers.io>
2018-07-19 16:31:27 +0100

    Logging aggregrated totals nicely - next to display them and save into a long term report

--- lib/pig_ci/loggers/memory.rb
+++ lib/pig_ci/loggers/memory.rb
@@ -10,4 +10,27 @@ class PigCi::Loggers::Memory
       f.puts([key, ::GetProcessMem.new.kb].join('|'))
     end
   end
+
+  def self.report!
+    aggregated_totals = {}
+
+    File.foreach(PigCi.tmp_directory.join('pig-ci-memory.txt')) do |f|
+      key, memory = f.strip.split('|')
+      memory = memory.to_i
+
+      aggregated_totals[key] ||= {
+        max: memory,
+        min: memory,
+        mean: memory,
+        number_of_requests: 0
+      }
+      aggregated_totals[key][:max] = memory if aggregated_totals[key][:max] < memory
+      aggregated_totals[key][:min] = memory if aggregated_totals[key][:max] > memory
+      aggregated_totals[key][:mean] = (memory + memory) / 2
+
+      aggregated_totals[key][:number_of_requests] += 1
+    end
+
+    puts aggregated_totals.inspect
+  end
 end
