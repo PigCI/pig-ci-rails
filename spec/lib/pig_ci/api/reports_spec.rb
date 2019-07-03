1bd327753aa26795902670b3288c1ed6e874630e
Mike Rogers <me@mikerogers.io>
2019-07-03 20:28:47 +0100

    100% code coverage with a few pendings for new stuff

--- spec/lib/pig_ci/api/reports_spec.rb
+++ spec/lib/pig_ci/api/reports_spec.rb
@@ -15,21 +15,47 @@ describe PigCI::Api::Reports do
   describe '#share!' do
     subject { api.share! }
 
-    before do
-      stub_request(:post, 'https://api.pigci.com/v1/reports')
-        .to_return(status: 200, body: '', headers: {})
+    context 'PigCI is offline' do
+      before do
+        stub_request(:post, 'https://api.pigci.com/v1/reports').to_timeout
+      end
+
+      it do
+        pending
+        expect { subject }.to output(/Unable to connect to PigCI API/).to_stdout
+      end
     end
 
-    it 'makes the API request with valid JSON as param' do
-      subject
+    context 'API key is invalid' do
+      before do
+        stub_request(:post, 'https://api.pigci.com/v1/reports')
+          .to_return(status: 401)
+      end
 
-      expect(a_request(:post, 'https://api.pigci.com/v1/reports').with do |req|
-        params = CGI.parse(req.body)
+      it do
+        pending
+        expect { subject }.to output('Unable to connect to PigCI API').to_stdout
+      end
+    end
 
-        expect(params['commit_sha1']).to eq(['test_sha1'])
-        expect(params['head_branch']).to eq(['test/branch'])
-        expect(params['reports[]'].first).to match_response_schema('reports')
-      end).to have_been_made
+    context 'request is successful' do
+      before do
+        stub_request(:post, 'https://api.pigci.com/v1/reports')
+          .to_return(status: 200)
+      end
+
+      it 'makes the API request with valid JSON as param, output link to report on PigCI' do
+        pending
+        expect { subject }.to output('View summary at https://pigci.com/project/test_sha1').to_stdout
+
+        expect(a_request(:post, 'https://api.pigci.com/v1/reports').with do |req|
+          params = CGI.parse(req.body)
+
+          expect(params['commit_sha1']).to eq(['test_sha1'])
+          expect(params['head_branch']).to eq(['test/branch'])
+          expect(params['reports[]'].first).to match_response_schema('reports')
+        end).to have_been_made
+      end
     end
   end
 end
