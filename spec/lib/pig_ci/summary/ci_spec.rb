a19ec8d8b96f49b4eb5ba148a62cd0dcd2f3da88
Mike Rogers <me@mikerogers.io>
2020-03-16 17:28:27 +0000

    Way better spec

--- spec/lib/pig_ci/summary/ci_spec.rb
+++ spec/lib/pig_ci/summary/ci_spec.rb
@@ -11,8 +11,20 @@ describe PigCI::Summary::CI do
     let(:profiler_sample_log_data) { File.open(File.join('spec', 'fixtures', 'files', 'profiler.txt')).read }
     subject { described_class.new(reports: reports).call! }
 
-    it 'Outputs the data to terminal screen' do
+    it 'Outputs the data to terminal screen without stopping CI' do
+      expect(Kernel).to_not receive(:exit)
       expect { subject }.to output(/translation missing: en.pig_ci.report.profiler.name/).to_stdout
+    end
+
+    context 'report is over the limit' do
+      before do
+        allow(reports[0]).to receive(:over_limit_for?).and_return(true)
+      end
+
+      it 'Outputs the data to terminal screen without stopping CI' do
+        expect(Kernel).to receive(:exit).with(2)
+        subject
+      end
     end
   end
 end
